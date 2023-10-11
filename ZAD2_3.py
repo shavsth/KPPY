6c0b61062161050a13250c6864c0d8d42e5aec79
Hanna <82726267+shavsth@users.noreply.github.com>
2023-10-12 01:24:08 +0200

    Update ZAD2_3.py

--- ZAD2_3.py
+++ ZAD2_3.py
@@ -1,10 +1,10 @@
-list_a = ['a', 34, 5.6, "abcd", 5, 23, 12.234, 'h']
-
 def intSum (list_var):
     sum = 0
-    for element in list_a:
+    for element in list_var:
         if type(element) == int:
             sum += element
     return sum
+
+list_a = ['a', 34, 5.6, "abcd", 5, 23, 12.234, 'h']
 
 print("Suma elementów typu int w tablicy to: {}".format(intSum(list_a)))
